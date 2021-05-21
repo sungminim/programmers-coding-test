d0991b2822e2ef205a6ba567d98cd89ac07ca87d
sungminim <__so1@hanmail.net>
2021-05-21 15:33:14 +0900

    level12 add

---
+++ level1/level1_12/main.js
@@ -0,0 +1,14 @@
+function solution(x, n) {
+    var answer = [];
+
+    for(i = 1; i <= n; i++){
+        var a = i * x;
+
+        answer.push(a)
+    }
+
+    console.log(answer)
+    return answer
+}
+
+
