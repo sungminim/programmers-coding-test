150a1d5160331057319ef33ae8701ddc316ba41d
sungminim <__so1@hanmail.net>
2021-05-25 14:20:44 +0900

    21-05-25 문제풀이

---
+++ level1/level1_14/main.js
@@ -0,0 +1,22 @@
+// Programmers 답안
+
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', data => {
+//     const n = data.split(" ");
+//     const a = Number(n[0]), b = Number(n[1]);
+//     var star= '*'
+
+//     for(var i=0; i<b; i++){
+//         document.write(star.repeat(a))
+//     }
+// });
+
+
+// function solution(a,b){
+//     var star= '*'
+//     for(var i=0; i<b; i++){
+//         document.write(star.repeat(a))
+//     }
+// }
+
+// solution(5,3)
